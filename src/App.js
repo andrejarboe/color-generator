3a8be1bb67dc55adb0e0813f402f5367582c9fb9
Andre Jarboe <andrejarboe@gmail.com>
2023-01-10 21:57:17 -0500

    error boarder works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+	const [color, setColor] = useState('');
+	const [isError, setError] = useState(false);
+	const [list, setList] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		try {
+			let colors = new Values(color).all(10);
+			console.log(colors);
+		} catch (error) {
+			setError(true);
+			console.log(error);
+		}
+	};
+
+	return (
+		<>
+			<section className="container">
+				<h3>Color Generator</h3>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={color}
+						onChange={(e) => setColor(e.target.value)}
+						className={`${isError ? 'error' : null}`}
+					/>
+					<button className="btn" type="submit" placeholder="#522d80">
+						submit
+					</button>
+				</form>
+			</section>
+			<section className="colors">
+				<h4>list goes here</h4>
+			</section>
+		</>
+	);
 }
 
-export default App
+export default App;
