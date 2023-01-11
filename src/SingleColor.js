f5a1c3eaf804d0d7cc8462e2465ddbdb45afbf8d
Andre Jarboe <andrejarboe@gmail.com>
2023-01-11 14:42:31 -0500

    use library to get hex

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import rgbToHex from './utils';
+// import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const [aleart, setAleart] = useState(false);
 	const bgc = rgb.join(',');
-	console.log(bgc);
-
-	const hex = rgbToHex(...rgb);
+	// const hex = rgbToHex(...rgb);
 
 	return (
 		<article className={`color`} style={{ backgroundColor: `rgb(${bgc})` }}>
 			<p className="percent-value">{weight}%</p>
-			<p className="color-value">{hex}</p>
+			<p className="color-value">{hexColor}</p>
 		</article>
 	);
 };
