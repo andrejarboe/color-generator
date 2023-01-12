507f4d2f4355dfb2bcaf5b248240f7f973c4f82d
Andre Jarboe <andrejarboe@gmail.com>
2023-01-11 19:32:27 -0500

    copied to clipboard removed after 3 seconds

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,6 +7,13 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const hexValue = `#${hexColor}`;
 	// const hex = rgbToHex(...rgb);
 
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAlert(false);
+		}, 3000);
+		return () => clearTimeout(timeout);
+	}, [alert]);
+
 	return (
 		<article
 			className={`color ${index > 10 && 'color-light'}`}
