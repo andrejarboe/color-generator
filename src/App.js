f101896df416287f5124124c814d89fd8a165110
Andre Jarboe <andrejarboe@gmail.com>
2023-01-11 19:33:28 -0500

    default purple

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js';
 function App() {
 	const [color, setColor] = useState('');
 	const [isError, setError] = useState(false);
-	const [list, setList] = useState([]);
+	const [list, setList] = useState(new Values('#522d80').all(10));
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
